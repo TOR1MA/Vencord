97fdf3d94845f0e10d3ad7e0ad744fc1eb8180f9
TOR1MA <76247724+TOR1MA@users.noreply.github.com>
2024-07-19 01:13:24 +0300

    Update Vencord.cmd

--- Vencord.cmd
+++ Vencord.cmd
@@ -1,43 +1,53 @@
 @echo off
 
-set /p UserInput= "1. Vencord | 2. Update plugins | 3. Exit and build: "
+set /p UserInput= "1. Install Vencord | 2. Update Vencord | 3. Install userplugins | 4. Update plugins | 5. Build: "
 	if /i "%UserInput%"=="1" (
-		set /p UserInput= "1. Install Vencord | 2. Update Vencord"
-			if /i "%UserInput%"=="1" (
-				cd %USERPROFILE%\Documents
-				git clone https://github.com/Vendicated/Vencord.git
-				cd Vencord
-				pnpm i
-				pnpm i -g pnpm
-				pnpm build
-				pnpm inject
-			)
-			if /i "%UserInput%"=="2" (
-				cd %USERPROFILE%\Documents\Vencord
-				pnpm inject
-			)
-		pause
+		cd %USERPROFILE%\Documents
+		git clone https://github.com/Vendicated/Vencord.git
+		cd Vencord
+		npm i -g pnpm
+		pnpm i
+		pnpm build
+		pnpm inject
+	pause
 )
 	if /i "%UserInput%"=="2" (
-		set /p UserInput= "1. gif collections | 2. message logger | 3. replace activities: "
-			if /i "%UserInput%"=="1" (
-				cd %USERPROFILE%\Documents\Vencord\src\userplugins\vc-gif-collections
-				git pull
-				pnpm build
-			)
-			if /i "%UserInput%"=="2" (
-				cd %USERPROFILE%\Documents\Vencord\src\userplugins\vc-message-logger-enhanced
-				git pull
-				pnpm build
-			)
-			if /i "%UserInput%"=="3" (
-				cd %USERPROFILE%\Documents\Vencord\src\userplugins\replaceActivityTypes
-				git pull
-				pnpm build
-			)
-		pause
+		cd %USERPROFILE%\Documents\Vencord
+		pnpm inject
+	pause
 )
 	if /i "%UserInput%"=="3" (
+		cd %USERPROFILE%\Documents\Vencord\src
+		git clone https://github.com/TOR1MA/userplugins
+		cd %USERPROFILE%\Documents\Vencord\src\userplugins
+		del readme.md
+		git clone https://github.com/Syncxv/vc-message-logger-enhanced
+		git clone https://github.com/Syncxv/vc-gif-collections
+		git clone https://github.com/nyakowint/replaceActivityTypes
+		git clone https://github.com/D3SOX/vc-betterActivities
+		pnpm build
+	pause
+)
+	if /i "%UserInput%"=="4" (
+				echo vc-gif-collections
+				cd %USERPROFILE%\Documents\Vencord\src\userplugins\vc-gif-collections
+				git pull
+				echo vc-message-logger-enhanced
+				cd %USERPROFILE%\Documents\Vencord\src\userplugins\vc-message-logger-enhanced
+				git pull
+				echo replaceActivityTypes
+				cd %USERPROFILE%\Documents\Vencord\src\userplugins\replaceActivityTypes
+				git pull
+				echo vc-betterActivities
+				cd %USERPROFILE%\Documents\Vencord\src\userplugins\vc-betterActivities
+				git pull
+				echo MoveEveryone and findReply
+				cd %USERPROFILE%\Documents\Vencord\src\userplugins
+				git pull
+				pnpm build
+	pause
+)
+	if /i "%UserInput%"=="5" (
 		cd %USERPROFILE%\Documents\Vencord
 		pnpm build
 	pause
